dfadc71df46bfb44520d7dadeb672584edd67463
Luis Vinicius <luisvinicius167@Mac-mini-de-Luis.local>
2017-12-28 13:39:02 -0200

    ok

--- dist/index.js
+++ dist/index.js
@@ -17,6 +17,13 @@ var getCursor = function getCursor() {
   return cursor;
 };
 
+var insertCursor = function insertCursor(element, cursor) {
+  return element.insertAdjacentElement('afterend', cursor);
+};
+var getElement = function getElement(el) {
+  return document.querySelector(el);
+};
+
 /**
  * @name setProps
  * @description Set the ityped properties configuration
@@ -29,7 +36,7 @@ var setProps = function setProps(_ref) {
       _ref$typeSpeed = _ref.typeSpeed,
       typeSpeed = _ref$typeSpeed === undefined ? 100 : _ref$typeSpeed,
       _ref$backSpeed = _ref.backSpeed,
-      backSpeed = _ref$backSpeed === undefined ? 100 : _ref$backSpeed,
+      backSpeed = _ref$backSpeed === undefined ? 300 : _ref$backSpeed,
       _ref$backDelay = _ref.backDelay,
       backDelay = _ref$backDelay === undefined ? 100 : _ref$backDelay,
       _ref$startDelay = _ref.startDelay,
@@ -37,118 +44,81 @@ var setProps = function setProps(_ref) {
       _ref$showCursor = _ref.showCursor,
       showCursor = _ref$showCursor === undefined ? true : _ref$showCursor,
       _ref$loop = _ref.loop,
-      loop = _ref$loop === undefined ? false : _ref$loop;
-  return { strings: strings, typeSpeed: typeSpeed, backSpeed: backSpeed, backDelay: backDelay, startDelay: startDelay, showCursor: showCursor, loop: loop };
+      loop = _ref$loop === undefined ? true : _ref$loop;
+  return {
+    strings: strings,
+    typeSpeed: typeSpeed,
+    backSpeed: backSpeed,
+    backDelay: backDelay,
+    startDelay: startDelay,
+    showCursor: showCursor,
+    loop: loop
+  };
 };
 
-var insertCursor = function insertCursor(element, cursor) {
-  return element.insertAdjacentElement('afterend', cursor);
-};
-var getElement = function getElement(el) {
-  return document.querySelector(el);
-};
+var startLoop = function startLoop(element, props) {
+  var times = [];
+  var strings = props.strings,
+      startDelay = props.startDelay,
+      typeSpeed = props.typeSpeed,
+      backSpeed = props.backSpeed,
+      backDelay = props.backDelay,
+      loop = props.loop;
 
+  var len = strings.length;
+  for (var i = 0; i < strings.length; i++) {
+    var _len = strings[i].length;
+    var nextTime = _len * typeSpeed + startDelay + _len * backSpeed + backDelay;
+    times.push(nextTime);
+    var time = i === 0 ? startDelay : times[i - 1];
+    startWriting(element, i, props, time);
+  }
+};
 var init = function init(el, config) {
   var props = setProps(config);
-  insertCursor(getElement(el), getCursor());
-  walkWithWords(el, props);
-};
-
-// const iterate = (s) => {
-//   const w = 
-// }
-
-// Pega o array e itera na primeira posição
-var walkWithWords = function walkWithWords(el, _ref2) {
-  var strings = _ref2.strings,
-      typeSpeed = _ref2.typeSpeed,
-      backSpeed = _ref2.backSpeed,
-      backDelay = _ref2.backDelay,
-      startDelay = _ref2.startDelay,
-      showCursor = _ref2.showCursor,
-      loop = _ref2.loop;
-
   var element = getElement(el);
-  var times = [];
-  return Promise.all(strings.map(function (string, index) {
-    return function (i) {
-      var len = string.length - 1;
-      var time = startDelay + len * typeSpeed + len * backSpeed;
-      times.push(time);
-      var t = window.setTimeout(function () {
-        return incrementWord(i, time = index === 0 ? startDelay : time, decrementWord, element, { strings: strings, typeSpeed: typeSpeed, backSpeed: backSpeed, backDelay: backDelay, startDelay: startDelay, showCursor: showCursor, loop: loop });
-      }, startDelay);
-    }(index);
-  }));
+  insertCursor(element, getCursor());
+  startLoop(element, props);
 };
 
-// const incrementWord = (
-//   i,
-//   time,
-//   el,
-//   { strings, typeSpeed, backSpeed, backDelay, startDelay, showCursor, loop }
-// ) => {
-//   return decrement => {
-//     window.setTimeout(() => {
-//       console.log('inc', time)
-//       Array.prototype.forEach.call(strings[i], (letter, index, arr) => {
-//         const t = window.setTimeout(() => {
-//           el.innerHTML += letter
-//           if (index === arr.length - 1) {
-//             window.clearTimeout(t)
-//             decrement(i, time, el, { strings, typeSpeed, backSpeed, backDelay, startDelay, showCursor, loop })
-//           }
-//         }, typeSpeed * index)
-//       }) 
-//     }, i === 0 ? startDelay : time + startDelay)
-//   }
-// }
-
-var incrementWord = function incrementWord(i, time, decrement, el, _ref3) {
-  var strings = _ref3.strings,
-      typeSpeed = _ref3.typeSpeed,
-      backSpeed = _ref3.backSpeed,
-      backDelay = _ref3.backDelay,
-      startDelay = _ref3.startDelay,
-      showCursor = _ref3.showCursor,
-      loop = _ref3.loop;
-
-  return new Promise(function (resolve, reject) {
-    window.setTimeout(function () {
-      Array.prototype.forEach.call(strings[i], function (letter, index, arr) {
-        var t = window.setTimeout(function () {
-          el.innerHTML += letter;
-          if (index === arr.length - 1) {
-            window.clearTimeout(t);
-            decrement(i, time, el, { strings: strings, typeSpeed: typeSpeed, backSpeed: backSpeed, backDelay: backDelay, startDelay: startDelay, showCursor: showCursor, loop: loop }, resolve);
-          }
-        }, typeSpeed * index);
-      });
-    }, time);
-  });
-};
-
-var decrementWord = function decrementWord(i, time, el, _ref4, resolve) {
-  var strings = _ref4.strings,
-      typeSpeed = _ref4.typeSpeed,
-      backSpeed = _ref4.backSpeed,
-      backDelay = _ref4.backDelay,
-      startDelay = _ref4.startDelay,
-      showCursor = _ref4.showCursor,
-      loop = _ref4.loop;
-
-  window.setTimeout(function () {
-    Array.prototype.forEach.call(strings[i], function (letter, index, arr) {
-      var position = el.innerHTML.length - index;
-      var tick = window.setTimeout(function () {
-        index === arr.length - 1 ? el.innerHTML = el.innerHTML.substring(0, position - 1) : el.innerHTML = el.innerHTML.substring(0, position);
-        if (index === arr.length - 1) {
-          window.clearTimeout(tick);
-          resolve();
-        }
-      }, backSpeed * index);
+var startWriting = function startWriting(el, position, props, time) {
+  var word = props.strings[position];
+  var startTick = window.setTimeout(function () {
+    Array.from(word).forEach(function (letter, i) {
+      return setTimeout(function () {
+        return typeString(word, i, el, props);
+      }, props.typeSpeed * (i + 1));
     });
   }, time);
+};
+
+var typeString = function typeString(word, i, el, props) {
+  if (i === word.length - 1) {
+    return window.setTimeout(function () {
+      var k = 0;
+      var tick = void 0;
+
+      var _loop = function _loop(l) {
+        k += 1;
+        tick = setTimeout(function () {
+          return eraseString(l, el, props, word);
+        }, props.backSpeed * k);
+      };
+
+      for (var l = word.length - 1; l >= 0; l--) {
+        _loop(l);
+      }
+      clearTimeout(tick);
+    }, props.backDelay);
+  }
+  el.innerHTML += word[i];
+};
+
+var eraseString = function eraseString(i, el, props, word) {
+  el.innerHTML = el.innerHTML.substring(0, --i);
+  if (i === 0 && props.strings.indexOf(word) === props.strings.length - 1 && props.loop) {
+    startLoop(el, props);
+  }
 };
 
 exports.init = init;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-
 const getCursor = () => {
   const cursor = document.createElement('span');
   cursor.classList.add('ityped-cursor');
   cursor.textContent = '|';
   return cursor
 }
+
+const insertCursor = (element, cursor) => element.insertAdjacentElement('afterend', cursor);
+const appendElement = parent => child => document.querySelector(parent).appendChild(child)
+const getElement = el => document.querySelector(el)
+
 
 /**
  * @name setProps
@@ -15,124 +19,76 @@ const getCursor = () => {
 const setProps = ({
   strings = ['AEIOU asiahsa skaonsa', 'SZsiasa'],
   typeSpeed = 100,
-  backSpeed = 100,
+  backSpeed = 300,
   backDelay = 100,
   startDelay = 100,
   showCursor = true,
-  loop = false
-}) => ({ strings, typeSpeed, backSpeed, backDelay, startDelay, showCursor, loop })
+  loop = true
+}) => ({
+  strings,
+  typeSpeed,
+  backSpeed,
+  backDelay,
+  startDelay,
+  showCursor,
+  loop
+})
 
-const insertCursor = (element, cursor) => element.insertAdjacentElement('afterend', cursor);
-const appendElement = parent => child => document.querySelector(parent).appendChild(child)
-const getElement = el => document.querySelector(el)
-
+const startLoop = (element, props) => {
+  const times = []
+  const { strings, startDelay, typeSpeed, backSpeed, backDelay, loop } = props
+  const len = strings.length
+  for (let i=0; i < strings.length; i++) {
+    const len = strings[i].length
+    const nextTime = (len * typeSpeed) + startDelay + ( len * backSpeed ) + backDelay
+    times.push(nextTime)
+    const time = i === 0 ? startDelay : times[i-1]
+    startWriting(element, i, props, time)
+  }
+}
 const init = (el, config) => {
   const props = setProps(config)
-  const initCursor = appendElement(el)
-  insertCursor(getElement(el), getCursor())
-  walkWithWords(el, props)
-}
-
-// const iterate = (s) => {
-//   const w = 
-// }
-
-// Pega o array e itera na primeira posição
-const walkWithWords = (el, { strings, typeSpeed, backSpeed, backDelay, startDelay, showCursor, loop }) => {
   const element = getElement(el)
+  const initCursor = appendElement(el)
+  insertCursor(element, getCursor())
   const times = []
-  return Promise.all(strings.map((string, index ) => {
-    return ((i) => {
-      const len = string.length - 1
-      let time = startDelay + (len * typeSpeed) + (len * backSpeed)
-      times.push(time)
-      const nextTime = times[i - 1]
-      const t = window.setTimeout(
-        () =>
-          incrementWord(
-            i,
-            time= index === 0 ? startDelay : time,
-            decrementWord,
-            element,
-            { strings, typeSpeed, backSpeed, backDelay, startDelay, showCursor, loop }
-          )
-        , startDelay)
-    })(index)
-  }))
+  startLoop(element, props)
 }
 
-// const incrementWord = (
-//   i,
-//   time,
-//   el,
-//   { strings, typeSpeed, backSpeed, backDelay, startDelay, showCursor, loop }
-// ) => {
-//   return decrement => {
-//     window.setTimeout(() => {
-//       console.log('inc', time)
-//       Array.prototype.forEach.call(strings[i], (letter, index, arr) => {
-//         const t = window.setTimeout(() => {
-//           el.innerHTML += letter
-//           if (index === arr.length - 1) {
-//             window.clearTimeout(t)
-//             decrement(i, time, el, { strings, typeSpeed, backSpeed, backDelay, startDelay, showCursor, loop })
-//           }
-//         }, typeSpeed * index)
-//       }) 
-//     }, i === 0 ? startDelay : time + startDelay)
-//   }
-// }
-
-const incrementWord = (
-  i,
-  time,
-  decrement,
-  el,
-  { strings, typeSpeed, backSpeed, backDelay, startDelay, showCursor, loop }
-) => {
-  return new Promise((resolve, reject) => {
-      window.setTimeout(() => {
-        Array.prototype.forEach.call(strings[i], (letter, index, arr) => {
-          const t = window.setTimeout(() => {
-            el.innerHTML += letter
-            if (index === arr.length - 1) {
-              window.clearTimeout(t)
-              decrement(
-                i, 
-                time,
-                el, 
-                { strings, typeSpeed, backSpeed, backDelay, startDelay, showCursor, loop },
-                resolve
-              )
-            }
-          }, typeSpeed * index)
-        }) 
-      }, time)
-  })
-}
-
-const decrementWord = (
-  i,
-  time,
-  el,
-  { strings, typeSpeed, backSpeed, backDelay, startDelay, showCursor, loop },
-  resolve
-) => {
-  window.setTimeout(() => {
-    Array.prototype.forEach.call(strings[i], (letter, index, arr) => {
-      const position = el.innerHTML.length - index
-      const tick = window.setTimeout(() => {
-        index === arr.length - 1
-          ? el.innerHTML = el.innerHTML.substring(0, position - 1)
-          : el.innerHTML = el.innerHTML.substring(0, position)
-        if (index === arr.length - 1) {
-          window.clearTimeout(tick)
-          resolve()
-        }
-      }, backSpeed * index)
-    })
+const startWriting = (el, position, props, time) => {
+  const word = props.strings[position]
+  let i = 0
+  const startTick = window.setTimeout(() => {
+    Array.from(word)
+      .forEach((letter, i) =>
+        setTimeout(() => typeString(word, i, el, props), props.typeSpeed * (i + 1))
+      )
   }, time)
-
 }
 
-export { init }
+const typeString = (word, i, el, props) => {
+  if (i === word.length - 1) {
+    return window.setTimeout(() => {
+      let k = 0
+      let tick
+      for (let l = word.length -1; l >= 0; l--) {
+        k += 1
+        tick = setTimeout(() => eraseString(l, el, props, word), props.backSpeed * k)
+      }
+      clearTimeout(tick)
+    }, props.backDelay)
+  }
+  el.innerHTML += word[i]
+}
+
+const eraseString = ( i, el, props, word ) => {
+  el.innerHTML = el.innerHTML.substring(0, --i)
+  if ( i === 0 && props.strings.indexOf(word) === props.strings.length - 1 && props.loop) {
+      startLoop(el, props)
+  }
+}
+
+
+export {
+  init
+}
